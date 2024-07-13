775061919e7dc4072dc9f5a56ebb47bc77218076
Taisiia <taechka97@gmail.com>
2024-07-13 21:07:12 +0200

    New project changes

--- src/main/java/com/example/training/domain/User.java
+++ src/main/java/com/example/training/domain/User.java
@@ -1,14 +1,17 @@
 package com.example.training.domain;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
